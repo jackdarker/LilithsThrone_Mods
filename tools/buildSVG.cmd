f0ee873418daa2f5c4f5dd4420413c32a5c0717f
Jack Darker <JackDarker@freenet.de>
2025-05-24 10:07:16 +0200

    fix

---
+++ tools/buildSVG.cmd
@@ -0,0 +1,4 @@
+
+D:
+cd D:\Projects\Javascript\SVGtoJS\node_modules\svgo\bin
+npx svgo --config=D:\Projects\Java\LilithsThrone_Mods\tools\svgoConfig.yml -r -f D:\Projects\Java\LilithsThrone_Mods\src D:\Projects\Java\LilithsThrone_Mods\res
